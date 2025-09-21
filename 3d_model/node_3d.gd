ab4f56d9e3684379c2753f22491926554944cc53
Kpoom66 <krittisak.n@kkumail.com>
2025-09-21 15:06:25 +0700

    Changes to be committed:

---
+++ 3d_model/node_3d.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"Strut Walking/AnimationPlayer".play("ShooterLib/walk")
+	$"Strut Walking2/AnimationPlayer".play("Locomotion-Library/attack1")
+	$"Strut Walking3/AnimationPlayer".play("ShooterLib/die1")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
