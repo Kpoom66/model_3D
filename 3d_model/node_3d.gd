00c02b0bc6e7eb46c4874b180785faeb6dd10b3c
Kpoom66 <krittisak.n@kkumail.com>
2025-09-21 15:18:39 +0700

    Changes to be committed:

--- 3d_model/node_3d.gd
+++ 3d_model/node_3d.gd
@@ -6,6 +6,8 @@ func _ready() -> void:
 	$"Strut Walking/AnimationPlayer".play("ShooterLib/walk")
 	$"Strut Walking2/AnimationPlayer".play("Locomotion-Library/attack1")
 	$"Strut Walking3/AnimationPlayer".play("ShooterLib/die1")
+	$"Strut Walking7/AnimationPlayer".play("ShooterLib/run_067")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
